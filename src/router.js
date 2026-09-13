6e06564fb08060f868184eba43743f9957707b10
Alexander Mendoza <jesusmendozav2111@gmail.com>
2026-09-12 20:48:12 -0500

    Add Swagger documentation and update API routes for books

--- src/router.js
+++ src/router.js
@@ -3,7 +3,45 @@ import { getBooksHandler, getBookByIdHandler } from './controllers/books.js';
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get all books
+ *     description: Returns the full list of books
+ *     responses:
+ *       200:
+ *         description: A list of books
+ *       500:
+ *         description: Unable to retrieve books
+ */
 router.get('/books', getBooksHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get a book by ID
+ *     description: Returns a single book matching the given ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The custom book ID, such as b1
+ *     responses:
+ *       200:
+ *         description: Book returned successfully
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Unable to retrieve the book
+ */
 router.get('/books/:id', getBookByIdHandler);
 
 export default router;
